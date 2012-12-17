d982b391b830233b6bc0a2195ec10d66351448b4
Michael Dillon <mic.dillon@gmail.com>
2012-12-17 11:53:01 +0100

    cleaned up make_cube

--- shapes.c
+++ shapes.c
@@ -29,23 +29,12 @@ gsu_cube make_cube(SGUfloat len) {
 		},
 		.index_count = 36,
 		.norms={},
-		.norm_count = 36,
-		// .norm_count = 8,
+		.norm_count = 8,
 		.tex_coords={},
-		// .tex_coord_count = 36
 		.tex_coord_count = 8
 	};
 
-	/** calculate norms */
-// #define v(i1,i2) add3(_c.verts[_c.indices[(i1)]], \
-// 		neg3(_c.verts[_c.indices[(i2)]]))
-// 	for (int i=0; i<_c.norm_count; i+=3) {
-// 		_c.norms[i+0] = norm3(cross3(v(i+2,i),v(i+1,i)));
-// 		_c.norms[i+1] = norm3(cross3(v(i,i+1),v(i+2,i+1)));
-// 		_c.norms[i+2] = norm3(cross3(v(i+1,i+2),v(i,i+2)));
-// 	}
-// #undef v
-
+	/** normals */
 	_c.norms[0] = (vec3){.v={0.0, 0.0, 1.0}};
 	_c.norms[1] = (vec3){.v={0.0, 0.0, 1.0}};
 	_c.norms[2] = (vec3){.v={0.0, 0.0, 1.0}};
@@ -56,20 +45,7 @@ gsu_cube make_cube(SGUfloat len) {
 	_c.norms[6] = (vec3){.v={0.0, 0.0, -1.0}};
 	_c.norms[7] = (vec3){.v={0.0, 0.0, -1.0}};
 
-	/** calculate texture coords */
-	// for (int i=0; i<_c.tex_coord_count; i+=6) {
-	// 	SGUfloat a = 0.0;
-	// 	SGUfloat b = 1.0;
-
-	// 	_c.tex_coords[i+0] = (vec2){.v={a, b}};
-	// 	_c.tex_coords[i+1] = (vec2){.v={b, b}};
-	// 	_c.tex_coords[i+2] = (vec2){.v={b, a}};
-
-	// 	_c.tex_coords[i+3] = (vec2){.v={a, b}};
-	// 	_c.tex_coords[i+4] = (vec2){.v={b, a}};
-	// 	_c.tex_coords[i+5] = (vec2){.v={a, a}};
-	// }
-
+	/** tex coords */
 	_c.tex_coords[0] = (vec2){.v={0.0, 1.0}};
 	_c.tex_coords[1] = (vec2){.v={1.0, 1.0}};
 	_c.tex_coords[2] = (vec2){.v={1.0, 0.0}};
